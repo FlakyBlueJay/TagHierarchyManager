90a9f56feba56b9dac82f22c925b5354a2454867
Flaky <git@flaky.blue>
2026-01-02 15:35:49 +0000

    Implement basic structure for the database settings window.

---
+++ UI/DatabaseSettingsWindow.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace UI;
+
+public partial class DatabaseSettingsWindow : Window
+{
+    public DatabaseSettingsWindow()
+    {
+        InitializeComponent();
+    }
+}
