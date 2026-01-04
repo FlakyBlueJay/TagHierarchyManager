de01424519d86785e9c57fbd86e2d97ae767f65c
Flaky <git@flaky.blue>
2026-01-04 16:31:55 +0000

    Implement basic database loading -> UI update in the MainWindowViewModel, add MessageBox.Avalonia for message boxes.

--- UI/ViewModels/MainWindowViewModel.cs
+++ UI/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,41 @@
-﻿namespace TagHierarchyManager.UI.ViewModels;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using TagHierarchyManager.Models;
+
+namespace TagHierarchyManager.UI.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    private TagDatabase _database;
+    
+    public ObservableCollection<Tag> Tags;
+
+    [ObservableProperty]
+    private bool _isDbLoaded;
+
+    // TODO searchViewModel
+    // TODO hierarchyViewModel
+    
+    public MainWindowViewModel()
+    {
+
+    }
+
+    public async Task LoadDatabase(string filePath)
+    {
+        TagDatabase db = new();
+        db.InitialisationComplete += (s, e) =>
+        {
+            Avalonia.Threading.Dispatcher.UIThread.Post(() =>
+            {
+                this._database = db;
+                this.IsDbLoaded = true;
+            });
+            Debug.WriteLine($"Database loaded on UI - name: {db.Name}, version: {db.Version}");
+        };
+        await db.LoadAsync(filePath);
+    }
 }
--- UI/Views/MainWindow.axaml.cs
+++ UI/Views/MainWindow.axaml.cs
@@ -1,15 +1,48 @@
+using System;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
+using TagHierarchyManager.UI.ViewModels;
 
 namespace TagHierarchyManager.UI.Views;
 
 public partial class MainWindow : Window
 {
+    public MainWindowViewModel? ViewModel => DataContext as MainWindowViewModel;
+    
     public MainWindow()
     {
         InitializeComponent();
     }
     
+    // TODO resx
+    private static FilePickerFileType TagDatabaseFileType { get; } = new("Tag Hierarchy Manager database")
+    {
+        Patterns = ["*.thdb"]
+    };
+    
     public void OpenAboutWindow(object? sender, RoutedEventArgs e) => new AboutWindow().ShowDialog(this);
     public void Quit(object? sender, RoutedEventArgs e) => this.Close();
+    
+    public async void MenuItemOpen_Click(object? sender, RoutedEventArgs e)
+    {
+        try
+        {
+            var files = await this.StorageProvider.OpenFilePickerAsync(
+                new FilePickerOpenOptions
+                {
+                    AllowMultiple = false,
+                    Title = "Open a tag database...",
+                    FileTypeFilter = [TagDatabaseFileType]
+                });
+            if (files.Count == 0) return;
+            var path = files[0].TryGetLocalPath();
+            if (path == null) return;
+            await this.ViewModel.LoadDatabase(path);
+        }
+        catch (Exception ex)
+        {
+            throw; // TODO handle exception
+        }
+    }
 }
